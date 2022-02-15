2cd030931af44142827891af3652f85a72ad2bea
Isaac <isaacandrespozo@gmail.com>
2022-02-15 12:19:14 -0500

    cambios en comentarios

--- src/App.js
+++ src/App.js
@@ -164,7 +164,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Material Dashboard 2"
+            brandName="Caja de ahorros"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
