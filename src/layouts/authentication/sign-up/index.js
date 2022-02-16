56b4424cf5e88a050a0764bad40ea7d8a25560bb
Isaac <isaacandrespozo@gmail.com>
2022-02-16 09:00:11 -0500

    Eliminando sing up

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -1,14 +1,8 @@
-// Authentication layout components
-import CoverLayout from "layouts/authentication/components/CoverLayout";
-
-// Images
-import bgImage from "assets/images/bg-sign-up-cover.jpeg";
-
 function Cover() {
   return (
-    <CoverLayout image={bgImage}>
-      <h1>Hola</h1>
-    </CoverLayout>
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
   );
 }
 
