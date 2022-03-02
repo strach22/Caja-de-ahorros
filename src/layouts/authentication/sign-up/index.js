b369d7cf5d6fb6c182586decbafb0096388da15c
SebasDiUks <sebastian.lovato@outlook.com>
2022-03-02 15:36:28 -0500

    Volver a empezar

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -1,5 +1,22 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { AddLista } from "./components/AddLista";
+
 function Cover() {
-  return <div />;
+  const [lista, setLista] = useState("");
+  return (
+    <>
+      <h2>Primera Aplicación de Prueba en el Sign-UP</h2>
+      <AddCategory setLista={setLista} />
+      <hr />
+
+      <ol>
+        {lista.map((auxLista) => (
+          <AddLista key={auxLista} auxLista={auxLista} />
+        ))}
+      </ol>
+    </>
+  );
 }
 
 export default Cover;
